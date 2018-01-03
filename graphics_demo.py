44a3e9ef9e10a02892f360b9ff50335166d1c193
WakasaHitomi <31477539+WakasaHitomi@users.noreply.github.com>
2018-01-03 09:29:47 -0500

    Add files via upload

--- graphics_demo.py
+++ graphics_demo.py
@@ -8,7 +8,7 @@ pygame.init()
 
 # Window
 SIZE = (800, 600)
-TITLE = "My Awesome Picture"
+TITLE = "Calm Day at Capsule corp."
 screen = pygame.display.set_mode(SIZE)
 pygame.display.set_caption(TITLE)
 
@@ -35,7 +35,13 @@ WBLUE = (208, 231, 239)
 SKY = (113, 206, 237)
 
 
-
+def draw_cloud(x, y):
+    pygame.draw.ellipse(screen, WHITE, [x, y + 20, 40 , 40])
+    pygame.draw.ellipse(screen, WHITE, [x + 60, y + 20, 40 , 40])
+    pygame.draw.ellipse(screen, WHITE, [x + 20, y + 10, 25, 25])
+    pygame.draw.ellipse(screen, WHITE, [x + 35, y, 50, 50])
+    pygame.draw.rect(screen, WHITE, [x + 20, y + 20, 60, 40])
+    
 # Game loop
 done = False
 
@@ -49,11 +55,20 @@ while not done:
 
     screen.fill(SKY)
 
-    #building shape
+    #building shape(make a black outline)
     pygame.draw.ellipse(screen, BEIGE, [100, 300, 600, 700])
     #grass
     pygame.draw.rect(screen, LGREEN, [0, 470, 800, 800])
-
+    #building chimeney(make black circle outline and then cover some in beige circle to make roundness)
+    pygame.draw.rect(screen, BEIGE, [230, 300, 80, 120])
+    pygame.draw.rect(screen, BEIGE, [460, 250, 80, 120])
+    #clouds
+    draw_cloud(50, 150)
+    draw_cloud(250, 75)
+    draw_cloud(350, 125)
+    draw_cloud(650, 100)
+    #palm tree recipie~~~~~~~~~~~~~~~~~~
+    
     # Update screen (Actually draw the picture in the window.)
     pygame.display.flip()
 
